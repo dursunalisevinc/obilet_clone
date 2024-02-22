3ce53d1f2b3e03a381a7164b4861b6dbebdcd8b4
dursunalisevinc <sevincdursunali61@gmail.com>
2024-02-22 19:02:45 +0300

    Layout ve Router Eklendi.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './layout/Layout';
+import Dashboard from './views/Dashboard';
+import NoPage from './views/NoPage';
+
 function App() {
   return (
-    <>
-    Obilet-React-Clone
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />}/>
+      </Route>
+      <Route path="*" element={<NoPage />}/>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ src/layout/Layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const Layout = () => {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
